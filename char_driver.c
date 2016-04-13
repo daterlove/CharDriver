e4e1910a5c22150b9c09efb0d3a57568060295fb
daterlove <306463830@qq.com>
2016-04-13 11:08:39 +0800

    LED测试

--- char_driver.c
+++ char_driver.c
@@ -13,6 +13,9 @@ static struct class *char_drv_class;
 static struct class_device	*char_drv_class_dev;
 int major;//保存主设备号
 
+volatile unsigned int *gpfcon = NULL;
+volatile unsigned int *gpfdat = NULL;
+
 static int char_drv_open(struct inode *inode, struct file *file)
 {
 
@@ -21,12 +24,12 @@ static int char_drv_open(struct inode *inode, struct file *file)
 
 static ssize_t char_drv_write(struct file *file, const char __user *buf, size_t count, loff_t * ppos)
 {
-	
+
 	return 0;
 }
 
 static struct file_operations char_drv_fops = {
-    .owner  =   THIS_MODULE,    /* 宏，指向编译模块时自动创建的__this_module变量 */
+    .owner  =   THIS_MODULE,    // 宏，指向编译模块时自动创建的__this_module变量 
     .open   =   char_drv_open,     
 	.write	=	char_drv_write,	   
 };
@@ -39,6 +42,14 @@ static int char_drv_init(void)
 	char_drv_class = class_create(THIS_MODULE, "char_drv");
 	char_drv_class_dev = class_device_create(char_drv_class, NULL, MKDEV(major, 0), NULL, "simple_char_drv"); /* 创建设备节点*/
 
+    //映射物理地址
+    gpfcon = (volatile unsigned long *)ioremap(0x56000050, 16);
+	gpfdat = gpfcon + 1;
+    
+    *gpfcon &= ~((0x3<<(4*2)) | (0x3<<(5*2)) | (0x3<<(6*2)));//设零
+	*gpfcon |= ((0x1<<(4*2)) | (0x1<<(5*2)) | (0x1<<(6*2)));//设一
+
+    *gpfdat &= ~((1<<4) | (1<<5) | (1<<6));
 	return 0;
 }
 
@@ -49,6 +60,8 @@ static void char_drv_exit(void)
 
 	class_device_unregister(char_drv_class_dev);
 	class_destroy(char_drv_class);
+    
+    iounmap(gpfcon);//接触MMU映射
 }
 
 module_init(char_drv_init);
